1d294534776b658b6bca6d6d31f20215fd960baf
Lucas L <lucasloffredo8@gmail.com>
2015-02-22 18:20:22 -0600

    changes

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This function will take the inverse of a matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  colNumber <- ncol(x)
+  y <- matrix(1, nrow(x))
+  for (i in 1:colNumber){
+    firstCol <- x[,i]
+    reversedCol <- rev(firstCol)
+    newMatrix <- cbind(reversedCol, x)
+  }
 }
 
 
-## Write a short comment describing this function
+## This function will take the inverse of the matrix
+## if it hasn't been cached or will just return the
+## cached value if it has been already.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- newMatrix
+  if(!is.null(m)){
+    message("getting cached data")
+    return(m)
+  } else {
+    colNumber <- ncol(x)
+    y <- matrix(1, nrow(x))
+    for (i in 1:colNumber){
+      firstCol <- x[,i]
+      reversedCol <- rev(firstCol)
+      newMatrix <- cbind(reversedCol, x)
+    }
+  }
 }
